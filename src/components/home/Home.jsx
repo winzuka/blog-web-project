06eee269f9a2196e03fe0aa5564dba8302bff116
winshuka <vinshukakannangara@gmail.com>
2024-08-19 17:44:13 +0530

    Show blog post on home component.

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import {blogPosts} from "../../utilities/blogsApi.jsx";
 
 const Home = () => {
     return (
-        <div>
-            <h1 className='text-5xl text-center mt-3 font-extrabold'>JavaScript Blogs</h1>
-        </div>
-    );
+        <>
+            {blogPosts.map(post=>(
+                <div>
+                    <div
+                        className="w-3/5 m-auto mt-7 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <a href="#">
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{post.title}</h5>
+                        </a>
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{post.content}</p>
+                        <button
+                            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">>
+                            Read more
+                            <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
+                                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                                      strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                            </svg>
+                        </button>
+
+
+                    </div>
+
+                </div>
+            ))}
+        </>
+
+
+    )
+        ;
 };
 
 export default Home;
