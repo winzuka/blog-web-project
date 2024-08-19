da299735aed45293207fdae137b609b770f5c3fc
winshuka <vinshukakannangara@gmail.com>
2024-08-19 19:58:52 +0530

    Completed the project.

--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -1,14 +1,14 @@
 import Home from "../components/home/Home.jsx";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Post from "../components/post/Post.jsx";
 
 const Router = () => {
-    return (
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/blog/:postID' element={<Post />}/>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/blog/:postID" element={<Post />} />
+    </Routes>
+  );
 };
 
 export default Router;
